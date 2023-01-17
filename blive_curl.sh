45704706046d7af577c126de5a8623dc8f9dbebb
candyraws <34184963+candyraws@users.noreply.github.com>
2023-01-17 19:17:28 +0800

    初次提交

---
+++ blive_curl.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+blive_room_id=$1
+sleep_min=3
+ua_setting="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36"
+
+blive_api="https://api.live.bilibili.com/xlive/web-room/v2/index/getRoomPlayInfo?room_id=${blive_room_id}&protocol=0&format=0&codec=0&qn=10000"
+
+# 开始前检测是否传入正确的房间号
+if [ -z "${blive_room_id}" ]; then
+    echo "$(date +%Y%m%d_%H%M%S): (6)检测到传入的room_id为空，已退出"
+    exit 6
+elif [ "$(echo -n "${blive_room_id}" | grep -o "[[:digit:]]" | wc -l)" != ${#blive_room_id} ]; then
+    echo "$(date +%Y%m%d_%H%M%S): (7)检测到room_id ${blive_room_id} 非纯数字，已退出"
+    exit 7
+fi
+
+while :; do
+    blive_api_back=$(curl -s6 --retry 2 --retry-delay 3 --connect-timeout 10 -A "${ua_setting}" "${blive_api}")
+    curl_status_code=$?
+    blive_status=$(echo "${blive_api_back}" | jq -r '.data.live_status')
+    if [ "${blive_status}" == "1" ]; then
+        blive_direct_url=$(echo "${blive_api_back}" | jq -jr '.data.playurl_info.playurl.stream[].format[] | select(.format_name=="flv").codec[]|.url_info[].host,.base_url,.url_info[].extra')
+        blive_ttl=$(echo "${blive_api_back}" | jq -r '.data.playurl_info.playurl.stream[].format[] | select(.format_name=="flv").codec[].url_info[].stream_ttl')
+        echo "$(date +%Y%m%d_%H%M%S): 直播中，拉起curl，开始录制，预期直播ttl: ${blive_ttl}"
+        blive_output_name="$(date +%Y%m%d_%H%M%S)_${blive_room_id}.flv"
+        curl -A "${ua_setting}" -e "https://live.bilibili.com/" "${blive_direct_url}" -o "${blive_output_name}"
+        curl2_status_code=$?
+        if [ "${curl2_status_code}" != "0" ]; then
+            echo "$(date +%Y%m%d_%H%M%S): curl2非正常退出，已停止录制，curl2错误码${curl2_status_code}"
+            exit 4
+        else
+            echo "$(date +%Y%m%d_%H%M%S): curl2已自动退出，可能是直播已结束或分段已经达到预期ttl"
+			test -s "${blive_output_name}" || rm "${blive_output_name}"
+        fi
+    elif [ "${blive_status}" == "2" ]; then
+        echo -ne "$(date +%Y%m%d_%H%M%S): 正在播放轮播视频，不录制, ${sleep_min}分钟后将再次检测\r"
+        sleep "${sleep_min}"m
+    elif [ "${blive_status}" == "0" ]; then
+        echo -ne "$(date +%Y%m%d_%H%M%S): 未开播, ${sleep_min}分钟后将再次检测\r"
+        sleep "${sleep_min}"m
+    elif [ ${curl_status_code} != "0" ]; then
+        echo "$(date +%Y%m%d_%H%M%S): (3) API获取失败，curl错误码${curl_status_code}"
+        exit 3
+    elif [ -z "${blive_status}" ]; then
+        echo "$(date +%Y%m%d_%H%M%S): (1) 直播间状态码为空，可能是jq或curl未安装"
+        exit 1
+    elif [ "${blive_status}" == "null" ]; then
+        echo "$(date +%Y%m%d_%H%M%S): (5) 直播间状态码检测失败，请检查直播间号 (${blive_room_id}) 是否正确或api是否失效"
+        exit 5
+    else
+        echo "$(date +%Y%m%d_%H%M%S): (2) 直播间状态码: ${blive_status};预期之外的错误"
+        exit 2
+    fi
+done
---
+++ readme.md
@@ -0,0 +1,67 @@
+# bililive-shell
+
+一个使用纯shell编写的bilibili直播录制工具，仅依赖`curl`和`jq`，你甚至可以在路由器上运行此脚本
+
+# 动机
+
+之前用某个bilibili直播录制工具录制一场较为重要的直播时，出bug了。
+
+录制了几个小时，最后只输出了一个文件夹，里面没有视频文件。并且，开始录制时，没有任何报错提示，当时想哭的心情都有了
+
+一气之下，自己写了这个bilibili直播录制脚本
+
+# 依赖
+
+`curl`: https://curl.se/
+
+`jq`: https://github.com/stedolan/jq
+
+也可以用各系统自带的包管理工具安装
+
+以debian为例
+
+```bash
+apt install curl jq -y
+```
+
+# 使用方法
+
+./blive_curl.sh 直播间号(支持短号)
+
+例如:
+
+```bash
+./blive_curl.sh 3394945
+```
+
+# TODO
+
+- [x] H.264编码的直播
+- [ ] H.265编码的直播
+- [x] 提前监控直播间
+- [x] 断线重新拉起
+
+# 一些闲话
+
+当主播网络不稳定时，可能会产生多个分段文件
+
+建议在录制后将flv转换为mp4封装
+
+Windows
+
+```cmd
+for %a in ("*.flv") do ffmpeg -i "%a" -c copy -f mp4 "%~na.mp4"
+```
+
+Linux
+
+```bash
+for i in $(ls *.flv);do ffmpeg -i ${i} -c copy -f mp4 ${i%.flv}.mp4;done
+```
+
+支持H.265编码的直播录制工具会放在另一个项目，主要使用ffmpeg录制
+
+
+
+如有bug，请提交issue
+
